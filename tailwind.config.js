8d84b815b93582b2493cd262cf1e0c1addd0ee7a
Orion Palmer <opalmer1989@gmail.com>
2022-12-02 10:53:21 -0500

    img routing change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [`./public/index.html`],
+  content: [`./index.html`],
   // content: ['./*.html'],
   theme: {
     screens: {
