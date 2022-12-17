0c1fb0dbfd1b2ccc6ff5ac7273d6eff7847b1227
Orion Palmer <opalmer1989@gmail.com>
2022-12-17 13:58:55 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+// let menuDrop = document.getElementsByClassName('menuStart')
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
     nav.classList.toggle('hidden')
+    // menuDrop.style.opacity = "1";
+    // menuDrop.style.transform = "translateY(0px)";
 })
+
+
+// // Select all slides
+// const slides = document.querySelectorAll(".slide");
+
+// // loop through slides and set each slides translateX property to index * 100% 
+// slides.forEach((slide, indx) => {
+//   slide.style.transform = `translateX(${indx * 100}%)`;
+// });
